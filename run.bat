ffc8716180cf53c20318561c1a75fbdcf9ddd8c1
Gorm Reventlow <gorm@reventlow.com>
2023-08-17 14:58:41 +0200

    webdriver update

--- run.bat
+++ run.bat
@@ -1,1 +1,12 @@
+@echo off
+
+REM Check if the destination directory exists
+if not exist "c:\chromedriver\" (
+    mkdir "c:\chromedriver\"
+)
+
+REM Copy the file, overwriting if it exists
+copy /Y "h:\chromedriver\chromedriver.exe" "c:\chromedriver\chromedriver.exe"
+
+REM Run the python script
 H:\infoscreen\venv\Scripts\python.exe H:\infoscreen\src\main.py
